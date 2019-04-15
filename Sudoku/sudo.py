674ceed24510ce716ee30468338eecac44ba186e
aidris823 <pokeanand@gmail.com>
2019-04-15 14:40:40 -0400

    Stockengarben

---
+++ Sudoku/sudo.py
@@ -0,0 +1,12 @@
+import sys, copy
+
+class Stack:
+    def __init__(self):
+def main(argv = None):
+    if not argv:
+        argv = sys.argv
+    name, board = getBoard(argv)
+    candidates = [1,2,3,4,5,6,7,8,9]
+    for i in board:
+        for j in i:
+            can = 
