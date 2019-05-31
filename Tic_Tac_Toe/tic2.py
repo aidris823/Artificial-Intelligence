d225750c43859de3136eea9cb3c2b164c7cd89e3
aidris823 <pokeanand@gmail.com>
2019-05-31 10:48:41 -0400

    Guggenheim

--- Tic_Tac_Toe/tic2.py
+++ Tic_Tac_Toe/tic2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 ''' Layout positions:
 0 1 2
 3 4 5
@@ -21,29 +23,79 @@ class BoardNode:
         print ('parents:',self.parents)
         print ('children:',self.children)
 
-def is_parent(child, parent):
-    c = list(child)
-    p = list(parent)
-    counter = 0
-    for i in range(len(c)):
-        if c[i] == p[i]:
-            counter += 1
-    return counter == 1
+def is_win(board):
+  for i, j, k in Wins:
+    if board[i] == board[k] and board[j] == board[i] and board[i] != '_':
+      return [True,board[i]]
+  return [False,None]
 
-def is_end(layout):
-    win_cliques = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
-    b = list(layout)
-    if "_" in layout:
-        return False
-    for i in win_cliques =
-        
-    
-        
-        
+def next_turn(turn):
+  if turn == 'x':
+    return 'o'
+  return 'x'
 
-
-
-    
 def CreateAllBoards(layout,parent):
     # recursive function to manufacture all BoardNode nodes and place them into the AllBoards dictionary
-    node = BoardNode(layout)
+    helper(layout,parent,-1,'x')
+
+def helper(layout,parent,pos,turn):
+  board = list(layout)
+
+  if pos == -1:
+    AllBoards[layout] = BoardNode(layout)
+    for i in range(9):
+      if board[i] == "_":
+        helper(layout,BoardNode(layout),i,turn)
+    return
+  board[pos] = turn
+  _board = "".join(board)
+ # print (_board)
+  node = None
+  if _board in AllBoards:
+    node = AllBoards[_board]
+  else: 
+    node = BoardNode(_board)
+    AllBoards[_board] = node
+  '''
+  if _board not in AllBoards:
+    node = BoardNode(_board)
+    AllBoards[_board] = node
+  else:
+    node = AllBoards[_board]
+    '''
+  if _board not in parent.children:
+    parent.children.append(_board)
+  node.parents.append(parent.layout)
+
+ 
+  states = is_win(board)
+  #print(states)
+  iswin = states[0]
+  winner = states[1]
+  if "_" not in board or iswin:
+    print ("rejio g")
+    if iswin:
+      if winner == 'x':
+        node.endState = 'x'
+        print ("fat safwana")
+      else: 
+        node.endState = 'o'
+        print ("fatty safwana")
+    else:
+      node.endState('d')
+      print ("fattest safwana")
+    return
+  
+  for i in range(9):
+    print (board[i])
+    print("rglt4lhig")
+    if board[i] == '_':
+      helper(_board,node,i,next_turn(turn))
+
+CreateAllBoards("_________",None)
+
+#print(AllBoards)
+print(len(AllBoards))
+x = 0
+o = 0
+d = 0
